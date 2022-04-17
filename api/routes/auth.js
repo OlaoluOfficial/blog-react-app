66533f1bb28893c78398a00ab59869b4ac3ef695
OlaoluOfficial <olaoluwasamuel96@yahoo.com>
2022-04-17 21:36:26 +0100

    add post route

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -21,3 +21,19 @@ router.post('/register', async (req, res) => {
     res.status(500).send(error);
   }
 });
+
+// Login
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(404).json('Wrong credentials');
+
+    const validated = await bcrypt.compare(req.body.password, user.password);
+    !validated && res.status(404).json('Wrong credentials');
+
+    const { password, ...rest } = user._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
