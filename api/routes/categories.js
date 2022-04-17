5c6efe0c96b9446b14d05a1343b8a3b9a7927df2
OlaoluOfficial <olaoluwasamuel96@yahoo.com>
2022-04-17 21:39:34 +0100

    add posst route

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -1,2 +1,13 @@
 const router = require('express').Router();
 const Category = require('../models/Category');
+
+// Create category
+router.post('/', async (req, res) => {
+  const newCat = new Category(req.body);
+  try {
+    const savedCat = await newCat.save();
+    res.status(200).json(savedCat);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
