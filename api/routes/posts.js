7418e8407f0b077ee1a6c5245bdcf2b86d6c6128
OlaoluOfficial <olaoluwasamuel96@yahoo.com>
2022-04-18 10:29:43 +0100

    post route

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,3 +1,14 @@
 const router = require('express').Router();
 const User = require('../models/User');
 const Post = require('../models/Post');
+
+// Create post
+router.post('/', async (req, res) => {
+  const newPost = new Post(req.body);
+  try {
+    const savedPost = await newPost.save();
+    res.status(201).json(savedPost);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
