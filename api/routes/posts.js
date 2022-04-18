ae24c77340706e9bb44909498d9bd78ad4ccd04c
OlaoluOfficial <olaoluwasamuel96@yahoo.com>
2022-04-18 10:32:48 +0100

    get by id

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -56,3 +56,11 @@ router.delete('/:id', async (req, res) => {
 });
 
 // Get Post
+router.get('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
