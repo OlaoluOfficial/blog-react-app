acd319b5c9ca30aeacce41643ca0d6ce7c02cfce
OlaoluOfficial <olaoluwasamuel96@yahoo.com>
2022-04-18 10:30:01 +0100

    post

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -3,12 +3,3 @@ const User = require('../models/User');
 const Post = require('../models/Post');
 
 // Create post
-router.post('/', async (req, res) => {
-  const newPost = new Post(req.body);
-  try {
-    const savedPost = await newPost.save();
-    res.status(201).json(savedPost);
-  } catch (error) {
-    res.status(400).json(error);
-  }
-});
