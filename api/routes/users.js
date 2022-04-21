d7a28d911f86210666ef86dc76f21fee26d99124
OlaoluOfficial <olaoluwasamuel96@yahoo.com>
2022-04-21 16:35:01 +0100

    add delete route

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
 
 /* GET user */
 router.get('/:id', async (req, res, next) {
@@ -8,10 +9,27 @@ router.get('/:id', async (req, res, next) {
     const { password, ...rest } = user._doc;
     res.status(200).json(rest);
   } catch (error) {
-    res.status(500).send(error);
+    res.status(500).json(error);
   }
 });
 
+// Delete
+router.delete('/:id', async (req, res) => {
+  if (req.body.userId === req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      if (user) {
+        await this.post.deleteMany({username: user.username});
+        await User.findByIdAndDelete(user);
+        res.status(500).json(error);
+      }
+    } catch (error) {
+      res.status(404).json('User not found...');
+    }
+  } else {
+    res.status(401).json('Unauthorized, you can only delete your own account');
+  }
+})
 
 
 module.exports = router;
