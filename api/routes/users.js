75f47e3ec7e73e9906c4816ab617f7e0479d046b
OlaoluOfficial <olaoluwasamuel96@yahoo.com>
2022-04-21 16:28:58 +0100

    update user route

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* GET user */
+router.get('/:id', async (req, res, next) {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...rest } = user._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
+
+
 
 module.exports = router;
