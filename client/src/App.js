bb0dffd2ef959bd3c28a071460c529bbcc1e871c
OlaoluOfficial <olaoluwasamuel96@yahoo.com>
2022-04-25 16:36:00 +0100

    checkpoint... switch issue

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,26 @@
+import TopBar from './components/topbar/TopBar';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+import Settings from './pages/settings/Settings';
+import Home from './pages/home/Home';
+import Single from './pages/single/Single';
+import Write from './pages/write/Write';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 function App() {
-  return <div className="App">blog app</div>;
+  return (
+    <Router>
+      <TopBar />
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
