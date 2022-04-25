e18c367030390a1e3c2b87511d35bfbd489c612e
OlaoluOfficial <olaoluwasamuel96@yahoo.com>
2022-04-25 16:19:54 +0100

    sidebar jsx

---
+++ client/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,39 @@
+import './sidebar.css';
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebarItem">
+        <span className="sidebarTitle">ABOUT ME</span>
+        <img
+          src="https://i.pinimg.com/236x/1e/3f/58/1e3f587572a7a7b20bbf1828595a1786--holiday-party-themes-holiday-gift-guide.jpg"
+          alt=""
+        />
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+          corporis odit dolores, ratione
+        </p>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">CATEGORIES</span>
+        <ul className="sidebarList">
+          <li className="sidebarListItem">Life</li>
+          <li className="sidebarListItem">Music</li>
+          <li className="sidebarListItem">Style</li>
+          <li className="sidebarListItem">Sport</li>
+          <li className="sidebarListItem">Tech</li>
+          <li className="sidebarListItem">Cinema</li>
+        </ul>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">FOLLOW US</span>
+        <div className="sidebarSocial">
+          <i className="sidebarIcon fa-brands fa-facebook-f"></i>
+          <i className="sidebarIcon fa-brands fa-twitter-square"></i>
+          <i className="sidebarIcon fa-brands fa-pinterest-p"></i>
+          <i className="sidebarIcon fa-brands fa-instagram-square"></i>
+        </div>
+      </div>
+    </div>
+  );
+}
