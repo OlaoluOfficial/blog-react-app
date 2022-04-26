0dde484158b95350315b4a4fd32bb2c97851ff5d
OlaoluOfficial <olaoluwasamuel96@yahoo.com>
2022-04-26 23:11:42 +0100

    settings

---
+++ client/src/pages/settings/Settings.jsx
@@ -0,0 +1,36 @@
+import './settings.css';
+import Sidebar from '../../components/sidebar/Sidebar';
+
+export default function Settings() {
+  return (
+    <div className="settings">
+      <div className="settingsWrapper">
+        <div className="settingsTitle">
+          <span className="settingsUpdateTitle">Update Your Account</span>
+          <span className="settingsDeleteTitle">DeleteAccount</span>
+        </div>
+        <form className="settingsForm">
+          <label>Profile Picture</label>
+          <div className="settingsPP">
+            <img
+              src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgd&dpr=2&w=500"
+              alt=""
+            />
+            <label htmlFor="fileInput">
+              <i className="settingsPPIcon fa-solid fa-user"></i>
+            </label>
+            <input type="file" id="fileInput" style={{ display: 'none' }} />
+          </div>
+          <label>Username</label>
+          <input type="text" placeholder='OG' />
+          <label>Email</label>
+          <input type="email" placeholder='olaolutheog@gmail.com' />
+          <label>Password</label>
+          <input type="password" />
+          <button className="settingsSubmit">Update</button>
+        </form>
+      </div>
+      <Sidebar />
+    </div>
+  );
+}
