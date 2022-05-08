bce8813678c34e6580b3fe826920dd4b69dda103
OlaoluOfficial <olaoluwasamuel96@yahoo.com>
2022-05-08 01:32:32 +0100

    update post

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,32 +1,25 @@
+import { Link } from 'react-router-dom';
 import './post.css';
 
-export default function Post() {
+export default function Post({ post }) {
   return (
     <div className="post">
-      <img
-        className="postImg"
-        src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-        alt=""
-      />
+      {post.photo && <img className="postImg" src={post.photo} alt="" />}
       <div className="postInfo">
         <div className="postCats">
-          <span className="postCat">Music</span>
-          <span className="postCat">Life</span>
+          {post.categories.map((c) => (
+            <span className="postCat">{c.name}</span>
+          ))}
         </div>
-        <span className="postTitle">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        </span>
+        <Link to={`/post/${post._id}`} className='link'>
+          <span className="postTitle">{post.title}</span>
+        </Link>
         <hr />
-        <span className="postDate">1 hour ago</span>
+        <span className="postDate">
+          {new Date(post.createdAt).toDateString()}
+        </span>
       </div>
-      <p className="postDesc">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus facere
-        delectus expedita cupiditate dolorum dolorem tempore animi repellendus
-        eaque consectetur, dicta, vero, harum iusto et voluptates ad explicabo
-        aliquid odit recusandae iste quis voluptatem voluptate? Recusandae
-        animi, exercitationem fuga maxime nostrum praesentium beatae possimus
-        voluptas nesciunt quia autem distinctio modi!
-      </p>
+      <p className="postDesc">{post.desc}</p>
     </div>
   );
 }
