d07dd0d37b0d3d929f1ab58eff5bc4835240b24d
OlaoluOfficial <olaoluwasamuel96@yahoo.com>
2022-05-09 19:18:47 +0100

    actions update

--- client/src/context/Actions.js
+++ client/src/context/Actions.js
@@ -15,3 +15,16 @@ export const Logout = () => ({
   type: 'LOGOUT'
 })
 
+export const UpdateStart = (userCredentials) => ({
+  type: "UPDATE_START",
+});
+
+export const UpdateSuccess = (user) => ({
+  type: "UPDATE_SUCCESS",
+  payload: user,
+});
+
+export const UpdateFailure = () => ({
+  type: "UPDATE_FAILURE",
+});
+
