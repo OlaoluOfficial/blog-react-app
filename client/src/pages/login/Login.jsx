6467fc94ee6678c85251baf03868e181f18443a4
OlaoluOfficial <olaoluwasamuel96@yahoo.com>
2022-05-09 19:19:06 +0100

    login

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,28 +1,28 @@
-import { useContext } from 'react';
-import { useRef } from 'react';
+import { useContext, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { Context } from '../../context/Context';
-import axios from 'axios'
+import axios from 'axios';
 import './login.css';
 
 export default function Login() {
-  const userRef = useRef()
-  const passwordRef = useRef()
-  const { dispatch, isFetching } = useContext(Context)
+  const userRef = useRef();
+  const passwordRef = useRef();
+  const { dispatch, isFetching } = useContext(Context);
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
-    dispatch({type: 'LOGIN_START'});
+    e.preventDefault();
+    dispatch({ type: 'LOGIN_START' });
     try {
       const res = await axios.post('/auth/login', {
         username: userRef.current.value,
         password: passwordRef.current.value,
-      })
-      dispatch({type: 'LOGIN_SUCCESS', payload: res.data})
+      });
+      dispatch({ type: 'LOGIN_SUCCESS', payload: res.data });
     } catch (error) {
-      dispatch({type: 'LOGIN_FAILURE'})
+      dispatch({ type: 'LOGIN_FAILURE' });
     }
-  }
+  };
+  
   return (
     <div className="login">
       <span className="loginTitle">Login</span>
@@ -41,10 +41,14 @@ export default function Login() {
           placeholder="Enter your password..."
           ref={passwordRef}
         />
-        <button className="loginButton" type='submit' disabled={isFetching}>Login</button>
+        <button className="loginButton" type="submit" disabled={isFetching}>
+          Login
+        </button>
       </form>
       <button className="loginRegisterButton">
-        <Link className='link' to="/register">Register</Link>
+        <Link className="link" to="/register">
+          Register
+        </Link>
       </button>
     </div>
   );
